0ff41a572617a910c4f1846d37733c58695181b7
Fika Febrika <fikafebrika02@gmail.com>
2022-10-15 13:10:23 +0700

    update navbar animation

--- js/main.js
+++ js/main.js
@@ -20,23 +20,10 @@
       $('.navbar').removeClass('navbar-dark');
       $('.navbar').addClass('navbar-light sticky-top shadow-sm');
     } else {
-      $('.navbar').removeClass('sticky-top shadow-sm');
+      $('.navbar').removeClass('navbar-light sticky-top shadow-sm');
       $('.navbar').addClass('navbar-dark');
     }
   });
-
-  // Back to top button
-  //   $(window).scroll(function () {
-  //     if ($(this).scrollTop() > 100) {
-  //       $('.back-to-top').fadeIn('slow');
-  //     } else {
-  //       $('.back-to-top').fadeOut('slow');
-  //     }
-  //   });
-  //   $('.back-to-top').click(function () {
-  //     $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-  //     return false;
-  //   });
 
   // Smooth scrolling on the navbar links
   $('.navbar-nav a').on('click', function (event) {
@@ -45,41 +32,15 @@
 
       $('html, body').animate(
         {
-          scrollTop: $(this.hash).offset().top - 150,
+          scrollTop: $(this.hash).offset().top - 60,
         },
         1500,
         'easeInOutExpo'
       );
-
-      if ($(this).parents('.navbar-nav').length) {
-        $('.navbar-nav .active').removeClass('active');
-        $(this).closest('a').addClass('active');
-      }
     }
   });
 
-  // Facts counter
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 2000,
-  });
-
-  // Testimonials carousel
-  $('.testimonial-carousel').owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    margin: 25,
-    dots: false,
-    loop: true,
-    nav: true,
-    navText: ['<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>'],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      992: {
-        items: 2,
-      },
-    },
+  $('body').on('click', function (event) {
+    $('.navbar-collapse').removeClass('show');
   });
 })(jQuery);
